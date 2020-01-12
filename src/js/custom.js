a8060f4063945b008bff072ad0c1d1b987f8dc1c
NavTheRaj <navrajkhanal61@gmail.com>
2020-01-12 16:44:54 +0545

    Added the export options in custom.js file

--- src/js/custom.js
+++ src/js/custom.js
@@ -16,246 +16,264 @@ var CURRENT_URL = window.location.href.split('#')[0].split('?')[0],
 
 // Sidebar
 $(document).ready(function() {
-    // TODO: This is some kind of easy fix, maybe we can improve this
-    var setContentHeight = function () {
-        // reset height
-        $RIGHT_COL.css('min-height', $(window).height());
+	// TODO: This is some kind of easy fix, maybe we can improve this
+	var setContentHeight = function () {
+		// reset height
+		$RIGHT_COL.css('min-height', $(window).height());
 
-        var bodyHeight = $BODY.outerHeight(),
-            footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
-            leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
-            contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
+		var bodyHeight = $BODY.outerHeight(),
+		footerHeight = $BODY.hasClass('footer_fixed') ? -10 : $FOOTER.height(),
+			leftColHeight = $LEFT_COL.eq(1).height() + $SIDEBAR_FOOTER.height(),
+			contentHeight = bodyHeight < leftColHeight ? leftColHeight : bodyHeight;
 
-        // normalize content
-        contentHeight -= $NAV_MENU.height() + footerHeight;
+		// normalize content
+		contentHeight -= $NAV_MENU.height() + footerHeight;
 
-        $RIGHT_COL.css('min-height', contentHeight);
-    };
+		$RIGHT_COL.css('min-height', contentHeight);
+	};
 
-    $SIDEBAR_MENU.find('a').on('click', function(ev) {
-        var $li = $(this).parent();
+	$SIDEBAR_MENU.find('a').on('click', function(ev) {
+		var $li = $(this).parent();
 
-        if ($li.is('.active')) {
-            $li.removeClass('active active-sm');
-            $('ul:first', $li).slideUp(function() {
-                setContentHeight();
-            });
-        } else {
-            // prevent closing menu if we are on child menu
-            if (!$li.parent().is('.child_menu')) {
-                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                $SIDEBAR_MENU.find('li ul').slideUp();
-            }
-            
-            $li.addClass('active');
+		if ($li.is('.active')) {
+			$li.removeClass('active active-sm');
+			$('ul:first', $li).slideUp(function() {
+				setContentHeight();
+			});
+		} else {
+			// prevent closing menu if we are on child menu
+			if (!$li.parent().is('.child_menu')) {
+				$SIDEBAR_MENU.find('li').removeClass('active active-sm');
+				$SIDEBAR_MENU.find('li ul').slideUp();
+			}
 
-            $('ul:first', $li).slideDown(function() {
-                setContentHeight();
-            });
-        }
-    });
+			$li.addClass('active');
 
-    // toggle small or large menu
-    $MENU_TOGGLE.on('click', function() {
-        if ($BODY.hasClass('nav-md')) {
-            $SIDEBAR_MENU.find('li.active ul').hide();
-            $SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
-        } else {
-            $SIDEBAR_MENU.find('li.active-sm ul').show();
-            $SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
-        }
+			$('ul:first', $li).slideDown(function() {
+				setContentHeight();
+			});
+		}
+	});
 
-        $BODY.toggleClass('nav-md nav-sm');
+	// toggle small or large menu
+	$MENU_TOGGLE.on('click', function() {
+		if ($BODY.hasClass('nav-md')) {
+			$SIDEBAR_MENU.find('li.active ul').hide();
+			$SIDEBAR_MENU.find('li.active').addClass('active-sm').removeClass('active');
+		} else {
+			$SIDEBAR_MENU.find('li.active-sm ul').show();
+			$SIDEBAR_MENU.find('li.active-sm').addClass('active').removeClass('active-sm');
+		}
 
-        setContentHeight();
+		$BODY.toggleClass('nav-md nav-sm');
 
-        $('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
-    });
+		setContentHeight();
 
-    // check active menu
-    $SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
+		$('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
+	});
 
-    $SIDEBAR_MENU.find('a').filter(function () {
-        return this.href == CURRENT_URL;
-    }).parent('li').addClass('current-page').parents('ul').slideDown(function() {
-        setContentHeight();
-    }).parent().addClass('active');
+	// check active menu
+	$SIDEBAR_MENU.find('a[href="' + CURRENT_URL + '"]').parent('li').addClass('current-page');
 
-    // recompute content when resizing
-    $(window).smartresize(function(){  
-        setContentHeight();
-    });
+	$SIDEBAR_MENU.find('a').filter(function () {
+		return this.href == CURRENT_URL;
+	}).parent('li').addClass('current-page').parents('ul').slideDown(function() {
+		setContentHeight();
+	}).parent().addClass('active');
 
-    setContentHeight();
+	// recompute content when resizing
+	$(window).smartresize(function(){  
+		setContentHeight();
+	});
 
-    // fixed sidebar
-    if ($.fn.mCustomScrollbar) {
-        $('.menu_fixed').mCustomScrollbar({
-            autoHideScrollbar: true,
-            theme: 'minimal',
-            mouseWheel:{ preventDefault: true }
-        });
-    }
+	setContentHeight();
+
+	// fixed sidebar
+	if ($.fn.mCustomScrollbar) {
+		$('.menu_fixed').mCustomScrollbar({
+			autoHideScrollbar: true,
+			theme: 'minimal',
+			mouseWheel:{ preventDefault: true }
+		});
+	}
 });
 
 // Panel toolbox
 $(document).ready(function() {
-    $('.collapse-link').on('click', function() {
-        var $BOX_PANEL = $(this).closest('.x_panel'),
-            $ICON = $(this).find('i'),
-            $BOX_CONTENT = $BOX_PANEL.find('.x_content');
-        
-        // fix for some div with hardcoded fix class
-        if ($BOX_PANEL.attr('style')) {
-            $BOX_CONTENT.slideToggle(200, function(){
-                $BOX_PANEL.removeAttr('style');
-            });
-        } else {
-            $BOX_CONTENT.slideToggle(200); 
-            $BOX_PANEL.css('height', 'auto');  
-        }
+	$('.collapse-link').on('click', function() {
+		var $BOX_PANEL = $(this).closest('.x_panel'),
+		$ICON = $(this).find('i'),
+		$BOX_CONTENT = $BOX_PANEL.find('.x_content');
 
-        $ICON.toggleClass('fa-chevron-up fa-chevron-down');
-    });
+		// fix for some div with hardcoded fix class
+		if ($BOX_PANEL.attr('style')) {
+			$BOX_CONTENT.slideToggle(200, function(){
+				$BOX_PANEL.removeAttr('style');
+			});
+		} else {
+			$BOX_CONTENT.slideToggle(200); 
+			$BOX_PANEL.css('height', 'auto');  
+		}
 
-    $('.close-link').click(function () {
-        var $BOX_PANEL = $(this).closest('.x_panel');
+		$ICON.toggleClass('fa-chevron-up fa-chevron-down');
+	});
 
-        $BOX_PANEL.remove();
-    });
+	$('.close-link').click(function () {
+		var $BOX_PANEL = $(this).closest('.x_panel');
+
+		$BOX_PANEL.remove();
+	});
 });
 // /Panel toolbox
 
 // Tooltip
 $(document).ready(function() {
-    $('[data-toggle="tooltip"]').tooltip({
-        container: 'body'
-    });
+	$('[data-toggle="tooltip"]').tooltip({
+		container: 'body'
+	});
 });
 // /Tooltip
 
 // Progressbar
 $(document).ready(function() {
 	if ($(".progress .progress-bar")[0]) {
-	    $('.progress .progress-bar').progressbar();
+		$('.progress .progress-bar').progressbar();
 	}
 });
 // /Progressbar
 
 // Switchery
 $(document).ready(function() {
-    if ($(".js-switch")[0]) {
-        var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-        elems.forEach(function (html) {
-            var switchery = new Switchery(html, {
-                color: '#26B99A'
-            });
-        });
-    }
+	if ($(".js-switch")[0]) {
+		var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+		elems.forEach(function (html) {
+			var switchery = new Switchery(html, {
+				color: '#26B99A'
+			});
+		});
+	}
 });
 // /Switchery
 
 // iCheck
 $(document).ready(function() {
-    if ($("input.flat")[0]) {
-        $(document).ready(function () {
-            $('input.flat').iCheck({
-                checkboxClass: 'icheckbox_flat-green',
-                radioClass: 'iradio_flat-green'
-            });
-        });
-    }
-// /iCheck
-
-// Table
-$('table input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('table input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-
-var checkState = '';
-
-$('.bulk_action input').on('ifChecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().addClass('selected');
-    countChecked();
-});
-$('.bulk_action input').on('ifUnchecked', function () {
-    checkState = '';
-    $(this).parent().parent().parent().removeClass('selected');
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifChecked', function () {
-    checkState = 'all';
-    countChecked();
-});
-$('.bulk_action input#check-all').on('ifUnchecked', function () {
-    checkState = 'none';
-    countChecked();
-});
-
-function countChecked() {
-    if (checkState === 'all') {
-        $(".bulk_action input[name='table_records']").iCheck('check');
-    }
-    if (checkState === 'none') {
-        $(".bulk_action input[name='table_records']").iCheck('uncheck');
-    }
-
-    var checkCount = $(".bulk_action input[name='table_records']:checked").length;
-
-    if (checkCount) {
-        $('.column-title').hide();
-        $('.bulk-actions').show();
-        $('.action-cnt').html(checkCount + ' Records Selected');
-    } else {
-        $('.column-title').show();
-        $('.bulk-actions').hide();
-    }
-}
-// Accordion
-    $(".expand").on("click", function () {
-        $(this).next().slideToggle(200);
-        $expand = $(this).find(">:first-child");
-
-        if ($expand.text() == "+") {
-            $expand.text("-");
-        } else {
-            $expand.text("+");
-        }
-    });
-
-// NProgress
-if (typeof NProgress != 'undefined') {
+	if ($("input.flat")[0]) {
 		$(document).ready(function () {
-				NProgress.start();
+			$('input.flat').iCheck({
+				checkboxClass: 'icheckbox_flat-green',
+				radioClass: 'iradio_flat-green'
+			});
+		});
+	}
+	// /iCheck
+
+	// Table
+	$('table input').on('ifChecked', function () {
+		checkState = '';
+		$(this).parent().parent().parent().addClass('selected');
+		countChecked();
+	});
+	$('table input').on('ifUnchecked', function () {
+		checkState = '';
+		$(this).parent().parent().parent().removeClass('selected');
+		countChecked();
+	});
+
+	var checkState = '';
+
+	$('.bulk_action input').on('ifChecked', function () {
+		checkState = '';
+		$(this).parent().parent().parent().addClass('selected');
+		countChecked();
+	});
+	$('.bulk_action input').on('ifUnchecked', function () {
+		checkState = '';
+		$(this).parent().parent().parent().removeClass('selected');
+		countChecked();
+	});
+	$('.bulk_action input#check-all').on('ifChecked', function () {
+		checkState = 'all';
+		countChecked();
+	});
+	$('.bulk_action input#check-all').on('ifUnchecked', function () {
+		checkState = 'none';
+		countChecked();
+	});
+
+	function countChecked() {
+		if (checkState === 'all') {
+			$(".bulk_action input[name='table_records']").iCheck('check');
+		}
+		if (checkState === 'none') {
+			$(".bulk_action input[name='table_records']").iCheck('uncheck');
+		}
+
+		var checkCount = $(".bulk_action input[name='table_records']:checked").length;
+
+		if (checkCount) {
+			$('.column-title').hide();
+			$('.bulk-actions').show();
+			$('.action-cnt').html(checkCount + ' Records Selected');
+		} else {
+			$('.column-title').show();
+			$('.bulk-actions').hide();
+		}
+	}
+	// Accordion
+	$(".expand").on("click", function () {
+		$(this).next().slideToggle(200);
+		$expand = $(this).find(">:first-child");
+
+		if ($expand.text() == "+") {
+			$expand.text("-");
+		} else {
+			$expand.text("+");
+		}
+	});
+
+	// NProgress
+	if (typeof NProgress != 'undefined') {
+		$(document).ready(function () {
+			NProgress.start();
 		});
 
 		$(window).on('load', function() {
-				NProgress.done();
+			NProgress.done();
 		});
-}
+	}
 
 
 
-		// For Exporting Button
-				$('#table_ack').DataTable({
-"dom": '<"row d-block"<"col-md-4"l><"col-md-4"><"col-md-4 ml-auto"f>>t<"bottom"pi>',
-						buttons: [
-												'copyHtml5',
-												'excelHtml5',
-												'csvHtml5',
-												'pdfHtml5',
-												'colvis'
+	// For Exporting Button
+	$('#table_ack').DataTable({
+		"dom": '<"row d-block"B<"col-md-4"l><"col-md-4"><"col-md-4 ml-auto"f>>t<"bottom"pi>',
+		buttons: [
+		{
+			extend:'csv',
+			exportOptions:{
+				columns:[0,1,2,3,4]
+			}
+		},
 
-										],
-								"pageLength": 20,
-								"fixedHeader":true,
-		});
+		{
+			extend:'excel',
+			exportOptions:{
+				columns:[0,1,2,3,4]
+			}
+		},
+		{
+			extend:'pdf',
+			exportOptions:{
+				columns:[0,1,2,3,4]
+			}
+		},
+		
+			
+
+		        
+			            
+		],
+		"pageLength": 20,
+		"fixedHeader":true,
+	});
 });
